5aa92a2b5cf0882ca4c8bcecb92ef3aba54af6ae
namnamu <skadlsrud8@naver.com>
2022-05-06 16:03:55 +0900

    add monster hurt

--- onion/Assets/Scripts/monsterHurted.cs
+++ onion/Assets/Scripts/monsterHurted.cs
@@ -2,54 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHurted : MonoBehaviour
+public class monsterHurted : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
     Transform transform;
-    Animator animator;
-    PlayerMoving movingScript;
-    PlayerAttacking attackScript;
-    public float knockback=0.1f;
+    //Animator animator;
+    monsterMoving movingScript;
+    public float knockback = 0.1f;
     private bool ishurted;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         transform = GetComponent<Transform>();
-        animator = GetComponent<Animator>();
-        movingScript = GetComponent<PlayerMoving>();
-        attackScript = GetComponent<PlayerAttacking>();
+        //animator = GetComponent<Animator>();
+        movingScript = GetComponent<monsterMoving>();
         ishurted = false;
     }
 
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-   
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (!ishurted && collision.gameObject.tag == "enemy")
+        if (!ishurted && collision.gameObject.tag == "playerAttack")
         {
+            Debug.Log("conflict!!");
             OnDamaged(collision.transform.position);//충돌한 상대 위치
         }
-
     }
 
     void OnDamaged(Vector2 targetPos)
     {
-        
-        animator.SetTrigger("isDamaged");
+
+        //animator.SetTrigger("isDamaged");
         //움직일 수 없어야함
         movingScript.enabled = false;
-        attackScript.enabled = false;
 
         //무적
         ishurted = true;
-        spriteRenderer.color = new Color(1, 1, 1, 0.4f); 
-        
+        spriteRenderer.color = new Color(1, 0, 0, 0.4f);
+
         //넉백
         int direction = transform.position.x - targetPos.x > 0 ? 1 : -1;//넉백 방향
-        transform.Translate(new Vector2(direction, 1)*knockback );//튕겨나감
+        transform.Translate(new Vector2(direction, 1) * knockback);//튕겨나감
 
 
 
@@ -64,6 +56,5 @@ public class PlayerHurted : MonoBehaviour
 
         //다시 움직임
         movingScript.enabled = true;
-        attackScript.enabled = true;
     }
 }
