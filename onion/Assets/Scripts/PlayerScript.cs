979899f9fa19347da10f3894649ec2aafc326390
Jeayuun <ghghwodbs@gmail.com>
2022-05-10 13:48:50 +0900

    Camera and Move

--- onion/Assets/Scripts/PlayerScript.cs
+++ onion/Assets/Scripts/PlayerScript.cs
@@ -4,20 +4,50 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float movementSpeed = 3.0f;
-    Vector2 movement = new Vector2();
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigid2D;
+
+    public float walkForce = 1.0f;
+    public float jumpForce = 5.0f;
+    public float maxWalkSpeed = 2.0f;
+
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        this.rigid2D = GetComponent<Rigidbody2D>();
     }
 
     //일정한 간격으로 호출하는 메서드
-    void FixedUpdate()
+    void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement.Normalize();//어느방향으로 움직이든 일정한 속력을 유지하도록 함.
-        rigidbody2D.velocity = movement * movementSpeed; //속도 설정
+        // jump하면서 key 누를 수 있게 바꾸기...
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        // right & left
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            key = 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            key = -1;
+        }
+
+        // player speed
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        // speed max
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        // move
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
     }
 }
